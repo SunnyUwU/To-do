432ef2346dfe51a4d4c0b23799f9924d2e67f5cd
Gybson Klarck <gybsonklarck@gmail.com>
2021-08-20 20:49:05 -0300

    Create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import TodoImg from "../assets/images/TodoImg.svg";
+import Logo from "../assets/images/Logo.svg";
+import googleIconImg from "../assets/images/google.svg";
+import githubIconImg from "../assets/images/github.svg";
+import Login from "../assets/images/login.svg";
+
+import { Button } from "../components/Button";
+import "../styles/auth.scss";
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={TodoImg} alt="To-do" />
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={Logo} alt="To-do" />
+                    <button className="button-login-with">
+                        <img src={googleIconImg} alt="Google" />
+                        Entrar com o Google
+                    </button>
+                    <button className="button-login-with">
+                        <img src={githubIconImg} alt="Google" />
+                        Entrar com o GitHub
+                    </button>
+                    <div className="separator">ou coloque seu nome</div>
+                    <form>
+                        <input 
+                            type="text" 
+                            placeholder="Digite seu nome"
+                            // value={username}
+                        />
+                        <Button type="submit">
+                            <img src={Login} alt="Entrar" />
+                            Entrar no To-do
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    );
+}
