63f7440ec9289f0214233e75e92d8478e3d52bb8
Gybson Klarck <gybsonklarck@gmail.com>
2021-08-20 20:49:14 -0300

    Create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean;
+}
+
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
+    return(
+        <button className={`button ${isOutlined ? "outlined" : ""}`} />
+    );
+}
