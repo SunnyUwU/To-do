a333891c5895a7c354ef054d126d269f85d7995f
Gybson Klarck <gybsonklarck@gmail.com>
2021-08-26 20:13:55 -0300

    Add todo route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
+import { Todo } from "./pages/Todo";
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/todo" exact component={Todo}/>
         </Switch>
     </BrowserRouter>
   );
